6670b4b5bdb09cc052dba00f25b484eda3c3f2c2
agimenez <agimenez@localhost>
2009-06-02 11:36:33 -0400

    Nuevo control de consulta por punto

--- web/public/media/scripts/openlayersmap.js
+++ web/public/media/scripts/openlayersmap.js
@@ -40,7 +40,7 @@ function olay_load() {
         type: 'jpg',
         getURL: osm_getTileURL,
         displayOutsideMaxExtent: true,
-        numZoomLevels: 5,
+        numZoomLevels: 6,
         maxResolution: 156543.0339/Math.pow(2,12)
     };
     
@@ -53,14 +53,52 @@ function olay_load() {
     map.addControl(new OpenLayers.Control.MousePosition());
     map.addControl(new OpenLayers.Control.KeyboardDefaults());
     map.addControl(new OpenLayers.Control.Navigation());
-    map.addControl(new OpenLayers.Control.NavToolbar());
+    //map.addControl(new OpenLayers.Control.NavToolbar());
     //map.addControl(new OpenLayers.Control.Permalink($('permalink')));
-    map.addControl(new OpenLayers.Control.Scale());
+    map.addControl(new OpenLayers.Control.ScaleLine());
     map.addControl(new OpenLayers.Control.Attribution());
     map.viewPortDiv.style.width = "";
     map.zoomToExtent(map.restrictedExtent)
     //map.setCenter(new OpenLayers.LonLat(-57.630001, -25.299999).transform(map.displayProjection, map.projection), 12)
     OpenLayers.Element.hide("msg");
+    
+    var control = new OpenLayers.Control();
+    toggled = false;
+    OpenLayers.Util.extend(control, {
+        draw: function () {
+            this.point = new OpenLayers.Handler.Point( control,
+                {"done": function(evt) { alert(evt) }});
+        }
+    });
+    
+    var button = new OpenLayers.Control.Button({
+        displayClass: "olControlPoint",
+        title: "Información Contextual",
+        trigger: function(evt) {
+            if (!toggled) {
+                this.activate()
+                control.point.activate();
+            }
+            else {
+                this.deactivate()
+                control.point.deactivate();
+            }
+            toggled = !toggled;
+            return true;
+        }
+    });
+    
+    var ms = new OpenLayers.Control.MouseDefaults(
+            {displayClass: "olControlNavigation", title:'Posicionamiento con el Mouse.'});
+    var panel = new OpenLayers.Control.Panel({defaultControl: ms, displayClass : "olControlNavToolbar"});
+    panel.addControls([
+        ms, 
+        new OpenLayers.Control.ZoomBox(
+            {displayClass : "olControlZoomBox", title:"Caja de Acercamiento: Realice click y arrastre para establecer la caja."}),
+        button
+    ]);
+    map.addControl(panel);
+    map.addControl(control);
 }
 
 function osm_getTileURL(bounds) {
