0eb9d8ab7020e7e98dcd88acfbece8920fd8074d
Alberto Gimenez <albergimenez@gmail.com>
2009-06-19 14:42:47 -0400

    Busqueda de Calles Alpha

--- web/main/views.py
+++ web/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
 from django.template import TemplateDoesNotExist, RequestContext
-from web.main import mapa, query
+from web.geo.models import Via
 
 def default(request, page):
     try:
@@ -15,8 +15,21 @@ def default(request, page):
 def xhr(request, page):
     try:
         if page == "search.xhr":
-            return render_to_response(page, query.search(request), mimetype="application/javascript; charset=iso8859-1")
+            #return render_to_response(page, query.search(request), mimetype="application/javascript; charset=iso8859-1")
+            return calle(request)
         else:
             return render_to_response(page, query.detail(request), mimetype="application/javascript; charset=iso8859-1")
     except TemplateDoesNotExist:
         raise Http404()
+    
+def calle(request):
+    params = request.REQUEST
+    if params.has_key("query"):
+        result = {}
+        result["resultado"] = True
+        nombre = params["query"].strip().replace(" "," & ")
+        result["items"] = Via.objects.extra(where=["to_tsvector('spanish', nombre) @@ to_tsquery(%s)"], params=[nombre])
+        result["count"] = len(result["items"])
+        return render_to_response("calle.html", result, mimetype="application/javascript; charset=iso8859-1")
+    else:
+        return render_to_response("calle.html", {"resultado" : False, "count" : 0}, mimetype="application/javascript; charset=iso8859-1")
