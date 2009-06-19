3efd8ebb79593537850665f8f6e0e246bc916fc1
agimenez <agimenez@localhost>
2009-06-19 13:36:26 -0400

    Busqueda de Calles

--- web/public/media/scripts/openlayersmap.js
+++ web/public/media/scripts/openlayersmap.js
@@ -2,6 +2,7 @@ var map;
 
 var overlays = {};
 var anyoverlay = false;
+var geoParser
 
 function goSearch(obj) {
   var obj = new Ajax.Request('search.xhr',
@@ -13,6 +14,46 @@ function goSearch(obj) {
             onLoading:function(request){OpenLayers.Element.hide('search_results');OpenLayers.Element.show('progress');},
             parameters:Form.serialize(obj)});
   return false;
+}
+
+function addGPolyline(id, detalle, points, xsw, ysw, xne, yne) {
+    if (anyoverlay) {
+        map.layers[1].destroyFeatures();
+        map.layers[2].clearMarkers();
+    }
+    
+    if (!overlays[id]) {
+        var sw = new OpenLayers.LonLat(xsw, ysw).transform(map.displayProjection, map.projection);
+        var ne = new OpenLayers.LonLat(xne, yne).transform(map.displayProjection, map.projection);
+        var geom = geoParser.read(points);
+        var bounds = new OpenLayers.Bounds(sw.lon, sw.lat, ne.lon, ne.lat);
+        overlays[id] = [geom, bounds];
+        anyoverlay = true;
+    }
+    else {
+        geom = overlays[id][0];
+        bounds = overlays[id][1];
+    }
+    map.zoomToExtent(bounds);
+    map.layers[1].setVisibility(false);
+    map.layers[2].setVisibility(false);
+    if (geom) {
+        map.layers[1].addFeatures(geom);
+    }
+    map.layers[2].addMarker(new OpenLayers.Marker.Box(bounds));
+    map.layers[2].setVisibility(true);
+    map.layers[1].setVisibility(true);
+    $('map').scrollTo();
+    
+    if (detalle && detalle != "None") {
+        var obj = new Ajax.Updater('detalle', 'doku.php',
+            {method:'get',
+            asynchronous:true,
+            parameters:{'id': detalle, 'do':'export_xhtmlbody'}});
+    }
+    else {
+        $('detalle').update("");
+    }
 }
 
 function olay_load() {
@@ -51,15 +92,11 @@ function olay_load() {
 
     map.addControl(new OpenLayers.Control.PanZoomBar());
     map.addControl(new OpenLayers.Control.MousePosition());
-    map.addControl(new OpenLayers.Control.KeyboardDefaults());
     map.addControl(new OpenLayers.Control.Navigation());
-    //map.addControl(new OpenLayers.Control.NavToolbar());
-    //map.addControl(new OpenLayers.Control.Permalink($('permalink')));
     map.addControl(new OpenLayers.Control.ScaleLine());
     map.addControl(new OpenLayers.Control.Attribution());
     map.viewPortDiv.style.width = "";
     map.zoomToExtent(map.restrictedExtent)
-    //map.setCenter(new OpenLayers.LonLat(-57.630001, -25.299999).transform(map.displayProjection, map.projection), 12)
     OpenLayers.Element.hide("msg");
     
     var control = new OpenLayers.Control();
@@ -99,6 +136,19 @@ function olay_load() {
     ]);
     map.addControl(panel);
     map.addControl(control);
+    
+    var layer_style = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
+    layer_style.fillOpacity = 0.2;
+    layer_style.graphicOpacity = 0.5;
+    layer_style.strokeColor = "#8B58F0";
+    layer_style.fillColor = "#8B58F0";
+    layer_style.strokeOpacity = 0.2;
+    layer_style.strokeWidth = 6;
+    var vectorLayer = new OpenLayers.Layer.Vector("Vector", {style: layer_style});
+    map.addLayer(vectorLayer);
+    var boxes  = new OpenLayers.Layer.Boxes("Boxes");
+    map.addLayer(boxes);
+    geoParser = new OpenLayers.Format.GeoJSON({externalProjection: map.displayProjection, internalProjection: map.projection});
 }
 
 function osm_getTileURL(bounds) {
